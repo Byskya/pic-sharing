d078f9968abaa4e03efbfaa23b0522218d1cb0b1
byskya <2514015411@qq.com>
2023-04-06 22:25:21 +0800

    Initial commit

---
+++ pic-sharing-springboot/src/main/java/com/example/picsharingspringboot/PicSharingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.picsharingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicSharingSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicSharingSpringbootApplication.class, args);
+    }
+
+}
